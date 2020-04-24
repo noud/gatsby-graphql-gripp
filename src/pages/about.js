05f4b8216e25b40d988525bba35935ecd63ba365
Noud de Brouwer <noud4@home.nl>
2020-04-24 15:32:56 +0200

    unused code

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Header from "../components/header"
+// import Header from "../components/header"
 
 
 export default ({ data }) => (
